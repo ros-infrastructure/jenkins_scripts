28bf46b2414a8968c846552725ee7479739d55fc
Ben Pitzer <ben.pitzer@gmail.com>
2012-12-07 19:47:41 -0800

    changed jenkins_code_quality to jenkins_scripts

--- analyze.py
+++ analyze.py
@@ -131,7 +131,7 @@ def analyze(ros_distro, stack_name, workspace, test_depends_on):
     	#for r in range(0, int(options.repeat)+1):
 	for r in range(0, int(0)+1):
 	    env['ROS_TEST_RESULTS_DIR'] = env['ROS_TEST_RESULTS_DIR'] + '/' + STACK_DIR + '_run_' + str(r)
-	    helper = subprocess.Popen(('%s/jenkins_code_quality/build_helper.py --dir %s build'%(workspace,STACK_DIR + '/' + stack_name)).split(' '), env=env)
+	    helper = subprocess.Popen(('%s/jenkins_scripts/build_helper.py --dir %s build'%(workspace,STACK_DIR + '/' + stack_name)).split(' '), env=env)
             helper.communicate()
             print "helper_return_code is: %s"%(helper.returncode)
 	    if helper.returncode != 0:
@@ -144,7 +144,7 @@ def analyze(ros_distro, stack_name, workspace, test_depends_on):
              'Set color to white')
 	    stack_dir = STACK_DIR + '/' + str(stack_name)
             filelist = stack_dir + '/filelist.lst'
-            helper = subprocess.Popen(('%s/jenkins_code_quality/concatenate_filelists.py --dir %s --filelist %s'%(workspace,stack_dir, filelist)).split(' '), env=env)
+            helper = subprocess.Popen(('%s/jenkins_scripts/concatenate_filelists.py --dir %s --filelist %s'%(workspace,stack_dir, filelist)).split(' '), env=env)
             helper.communicate()
             print 'Concatenate filelists done --> %s'%str(stack_name) 
              
@@ -157,7 +157,7 @@ def analyze(ros_distro, stack_name, workspace, test_depends_on):
             # export metrics to yaml and csv files
 	    print 'stack_dir: %s '%str(stack_dir)
 	    print 'stack_name[0]: %s '%str(stack_name)
-            helper = subprocess.Popen(('%s/jenkins_code_quality/export_metrics_to_yaml.py --path %s --doc doc --csv csv --config %s/jenkins_code_quality/export_config_roscon.yaml'%(workspace,stack_dir,workspace)).split(' '), env=env)
+            helper = subprocess.Popen(('%s/jenkins_scripts/export_metrics_to_yaml.py --path %s --doc doc --csv csv --config %s/jenkins_scripts/export_config_roscon.yaml'%(workspace,stack_dir,workspace)).split(' '), env=env)
             helper.communicate()
 	    call('echo -e "\033[33;0m Color Text"', env,
              'Set color to white')
@@ -166,7 +166,7 @@ def analyze(ros_distro, stack_name, workspace, test_depends_on):
             # push results to server
 	    print 'stack_dir: %s '%str(stack_dir)
 	    print 'stack_name[0]: %s '%str(stack_name)
-            helper = subprocess.Popen(('%s/jenkins_code_quality/push_results_to_server.py --path %s --doc doc'%(workspace,stack_dir)).split(' '), env=env)
+            helper = subprocess.Popen(('%s/jenkins_scripts/push_results_to_server.py --path %s --doc doc'%(workspace,stack_dir)).split(' '), env=env)
             helper.communicate()
 	    call('echo -e "\033[33;0m Color Text"', env,
              'Set color to white')
