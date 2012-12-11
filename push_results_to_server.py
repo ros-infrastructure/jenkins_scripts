20440c24099a8dbc2d81e8f95df941c8a5d2b389
Ben Pitzer <ben.pitzer@gmail.com>
2012-12-11 15:33:41 -0800

    changed ssh keypair filename

--- push_results_to_server.py
+++ push_results_to_server.py
@@ -17,7 +17,7 @@ from jobs_common import *
 # Global settings
 env = get_environment()
 env['INSTALL_DIR'] = os.getcwd()
-WIKI_SERVER_KEY_PATH = os.environ['HOME'] +'/chroot_configs/ec2-keypair.pem'
+WIKI_SERVER_KEY_PATH = os.environ['HOME'] +'/chroot_configs/keypair.pem'
 ROS_WIKI_SERVER = 'ubuntu@ec2-184-169-231-58.us-west-1.compute.amazonaws.com:~/doc'
       
 def get_options(required, optional):
