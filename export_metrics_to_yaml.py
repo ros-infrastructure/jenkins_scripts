bcebf7550a46f6b9649a67a4e4557fc66eba922d
Ben Pitzer <ben.pitzer@gmail.com>
2012-12-11 16:29:18 -0800

    remove env variable when calling subprocess cloc

--- export_metrics_to_yaml.py
+++ export_metrics_to_yaml.py
@@ -241,7 +241,7 @@ class ExportYAML:
                
     def create_loc(self):
         filename = self.doc + '/' + 'code_quantity.yaml'
-	helper = subprocess.Popen(('cloc.pl %s --not-match-d=build --yaml --out %s'%(self.path, filename)).split(' '),env=env)
+	helper = subprocess.Popen(('cloc.pl %s --not-match-d=build --yaml --out %s'%(self.path, filename)).split(' '))
         helper.communicate()
                       
     def export(self):
