ba652057e04bc95fead84507db536ce2a517686c
kuj1pal <kuehnjoh@gmail.com>
2013-01-17 13:12:54 -0800

    extended to fuerte/groovy analysis

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -ex
 /bin/echo '^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ run_analysis.sh ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^'
 
 # Add ros sources to apt
@@ -7,14 +7,48 @@ wget http://packages.ros.org/ros.key -O $WORKSPACE/ros.key
 apt-key add $WORKSPACE/ros.key
 sudo apt-get update
 
+
 # install stuff we need
 echo "Installing Debian packages we need for running this script"
-sudo apt-get install apt-utils ia32-libs python-rosinstall python-rospkg python-tk openssh-server ros-electric-ros-release --yes
+if [ "$ROS_DISTRO" == 'electric' ] ; then
+    echo "Using rosdistro electric"
+    sudo apt-get install apt-utils ia32-libs python-rosinstall python-rospkg python-tk openssh-server ros-electric-ros-release --yes
+    source /opt/ros/$ROS_DISTRO/setup.sh
+    sudo cp $HOME/chroot_configs/rostoolchain.cmake /opt/ros/$ROS_DISTRO/ros/rostoolchain.cmake
 
-source /opt/ros/$ROS_DISTRO/setup.sh
+    # call analysis
+    python $WORKSPACE/jenkins_scripts/analyze.py $ROS_DISTRO $STACK_NAME
+
+elif [ "$ROS_DISTRO" == 'fuerte' ] ; then
+    echo "Using rosdistro fuerte"
+    # ia32-libs
+    sudo apt-get install apt-utils python-rosinstall python-rosdep python-rospkg python-tk openssh-server ros-fuerte-ros --yes
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    svn co https://code.ros.org/svn/ros/stacks/ros_release/trunk $HOME/fuerte_workspace/ros_release
+    export ROS_PACKAGE_PATH=$HOME/fuerte_workspace:$ROS_PACKAGE_PATH
+    cd $HOME/fuerte_workspace/ros_release && rosmake
+    sudo cp $HOME/chroot_configs/rostoolchain.cmake /opt/ros/$ROS_DISTRO/share/ros/rostoolchain.cmake
+
+    # call analysis
+    python $WORKSPACE/jenkins_scripts/analyze_fuerte_groovy.py $ROS_DISTRO $STACK_NAME
+
+elif [ "$ROS_DISTRO" == 'groovy' ] ; then
+    echo "Using rosdistro groovy"
+    # ia32-libs
+    sudo apt-get install apt-utils python-rosinstall python-rosdep python-rospkg python-tk openssh-server ros-groovy-ros-base --yes
+    sudo rosdep init
+    rosdep update
+    source /opt/ros/$ROS_DISTRO/setup.bash
+    #svn co https://code.ros.org/svn/ros/stacks/ros_release/trunk $HOME/groovy_workspace/ros_release
+    svn co https://code.ros.org/svn/ros/stacks/ros_release/branches/groovy/ $HOME/groovy_workspace/ros_release
+    export ROS_PACKAGE_PATH=$HOME/groovy_workspace:$ROS_PACKAGE_PATH
+    cd $HOME/groovy_workspace/ros_release && rosmake
+    sudo cp $HOME/chroot_configs/rostoolchain.cmake /opt/ros/$ROS_DISTRO/share/ros/core/rosbuild/rostoolchain.cmake
+    cd $WORKSPACE
+    # call analysis
+    python $WORKSPACE/jenkins_scripts/analyze_fuerte_groovy.py $ROS_DISTRO $STACK_NAME
+fi
+
+
+
 source $HOME/chroot_configs/set_qacpp_path.sh
-
-sudo cp $HOME/chroot_configs/rostoolchain.cmake /opt/ros/$ROS_DISTRO/ros/rostoolchain.cmake
-
-# call analysis
-python $WORKSPACE/jenkins_scripts/analyze.py $ROS_DISTRO $STACK_NAME
