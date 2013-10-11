995a01627986d9d65f704d58497e1f61e0ea5069
Tully Foote <tfoote@willowgarage.com>
2013-10-11 16:53:42 -0700

    removing unused RosDep class which had apt-get install commands

--- rosdep.py
+++ rosdep.py
@@ -55,35 +55,3 @@ class RosDepResolver:
 
     def has_apt(self, apt_entry):
         return apt_entry in self.a2r
-
-
-class RosDep:
-    def __init__(self, ros_distro):
-        self.r2a = {}
-        self.a2r = {}
-        self.env = os.environ
-        self.env['ROS_DISTRO'] = ros_distro
-
-        # Initialize rosdep database
-        print "Ininitalize rosdep database"
-        call("apt-get install --yes lsb-release python-rosdep")
-        call("rosdep init", self.env)
-        call("rosdep update", self.env)
-
-    def to_apt(self, r):
-        if r in self.r2a:
-            return self.r2a[r]
-        else:
-            res = check_output("rosdep resolve %s" % r, self.env).split('\n')
-            if len(res) == 1:
-                raise Exception("Could not resolve rosdep")
-            a = check_output("rosdep resolve %s" % r, self.env).split('\n')[1]
-            print "Rosdep %s resolved into %s" % (r, a)
-            self.r2a[r] = a
-            self.a2r[a] = r
-            return a
-
-    def to_stack(self, a):
-        if not a in self.a2r:
-            print "%s not in apt-to-rosdep cache" % a
-        return self.a2r[a]
